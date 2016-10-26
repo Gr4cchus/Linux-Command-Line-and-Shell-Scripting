e311240414e19b5fd294c9d117dae712138ab7e7
Gr4cchus <ahivent@gmail.com>
2016-10-26 06:03:29 -0500

    User dependent script almost completed. Implement mechanism to label logs according to user, and cat properly.

--- CDC16/Gather-user-logs.sh
+++ CDC16/Gather-user-logs.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env bash
+# when using [[ ]] dont forget to use bash instead of !/bin/sh
 
 echo "test"
-## BELOW THIS POINT - user specific
-#cat ~/.bash_history > $(whoami)-bash_history.txt
-#if [ $? -eq 0 ]; then
-#    echo "Good:bash_history obtained"
-#else
-#    echo "Failed"
-#fi
+# Read a file (data stream, variable) line-by-line or field by field
+#while IFS= read -r line; do
+#    printf '%s\n' "$line"
+#done < "$file"
 
-for i in $(ls)
-do
-    echo "$i"
+
+grep /home /etc/passwd | cut -d':' -f6 - | while IFS= read -r line; do
+    printf '%s\n' "$line"
+    cd "$line"
+    for FILE in $(ls -a); do
+        if [ -f "$FILE" ] && [[ "$FILE" == .bash* ]]; then    # the conditional order must be as follow. [[]] lack portability but allows better expansion
+            echo "found file: $FILE"
+            cat "$FILE" > file.txt
+        elif [ -f "$FILE" ] && [[ "$FILE" == .mysql* ]]; then
+            echo "found file: $FILE"
+        elif [ -f "$FILE" ] && [[ "$FILE" == .vim* ]]; then
+            echo "found file: $FILE"
+        elif [ -f "$FILE" ] && [[ "$FILE" == .pro* ]]; then
+            echo "found file: $FILE"
+        elif [ -f "$FILE" ] && [[ "$FILE" == *.sql ]]; then
+            echo "found file: $FILE"
+        elif [ -f "$FILE" ] && [[ "$FILE" == *.log ]]; then
+            echo "found file: $FILE"
+        elif [ -f "$FILE" ] && [[ "$FILE" == .emac* ]]; then
+            echo "found file: $FILE"
+        fi
+    done
 done
