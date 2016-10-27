c681913292565c11d6b5f5331cb5c76b6a580a97
Gr4cchus <ahivent@gmail.com>
2016-10-26 21:36:19 -0500

    Complete rewrite was required  as "ls" should not be used for listing files; better for listing attributes of a single file. Echo was also causing some issues.

--- CDC16/Gather-user-logs.sh
+++ CDC16/Gather-user-logs.sh
@@ -1,32 +1,67 @@
 #!/usr/bin/env bash
 # when using [[ ]] dont forget to use bash instead of !/bin/sh
 
-echo "test"
 # Read a file (data stream, variable) line-by-line or field by field
 #while IFS= read -r line; do
 #    printf '%s\n' "$line"
 #done < "$file"
 
-
+# Grab home directories from passwd by cut delimited by : , then read each line
 grep /home /etc/passwd | cut -d':' -f6 - | while IFS= read -r line; do
-    printf '%s\n' "$line"
+    # Be weary of using echo inserting unwanted and difficult to remove special characters
+#    printf '%s\n' "$line"
     cd "$line"
-    for FILE in $(ls -a); do
-        if [ -f "$FILE" ] && [[ "$FILE" == .bash* ]]; then    # the conditional order must be as follow. [[]] lack portability but allows better expansion
-            echo "found file: $FILE"
-            cat "$FILE" > file.txt
+    shopt -s dotglob
+    for FILE in *; do
+        # the conditional order must be as follow (-f xxx). [[]] lack portability but allows better expansion
+        if [[ -f "$FILE" ]] && [[ "$FILE" == .bash* ]]; then
+#            printf 'Found "%s"\n' "$FILE"
+            cat "$FILE" > "$line$FILE.txt"
         elif [ -f "$FILE" ] && [[ "$FILE" == .mysql* ]]; then
-            echo "found file: $FILE"
+            cat "$FILE" > "$line$FILE.txt"
         elif [ -f "$FILE" ] && [[ "$FILE" == .vim* ]]; then
-            echo "found file: $FILE"
+            cat "$FILE" > "$line$FILE.txt"
         elif [ -f "$FILE" ] && [[ "$FILE" == .pro* ]]; then
-            echo "found file: $FILE"
+            cat "$FILE" > "$line$FILE.txt"
         elif [ -f "$FILE" ] && [[ "$FILE" == *.sql ]]; then
-            echo "found file: $FILE"
+            cat "$FILE" > "$line$FILE.txt"
         elif [ -f "$FILE" ] && [[ "$FILE" == *.log ]]; then
-            echo "found file: $FILE"
+            cat "$FILE" > "$line$FILE.txt"
         elif [ -f "$FILE" ] && [[ "$FILE" == .emac* ]]; then
-            echo "found file: $FILE"
+            cat "$FILE" > "$line$FILE.txt"
         fi
     done
+    printf 'Found %s\n' "$line"
 done
+
+# Archive files -------------------------
+echo
+echo "Would you like to compress all these files for easy transfer?"
+read -p "Press y or n (lowercase): " userChoice1
+
+if [ "$userChoice1" = "y" ]; then
+    cd /home; tar cvf /home/Forencics-Logs.tar *.*
+    echo "Compression completed"
+    echo
+else
+    echo "No compression selected"
+    echo
+fi
+
+# Cleanup -------------------------
+echo "Would you like to remove all these newly created files?"
+read -p "Press y or n (lowercase): " userChoice2
+
+if [ "$userChoice2" = "y" ]; then
+    for file in "/home/"*.txt; do
+        if [[ -f "$file" ]]; then
+#            printf 'Removing file "%s"\n' "$file"
+            rm rm -f "$file"
+        fi
+    done
+    echo "Cleanup completed. Apu: Thank you come again"
+else
+    echo "Removal not selected"
+fi
+
+read -p "Check /home for files"
